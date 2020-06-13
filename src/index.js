cff769fd6a5c1561a0650322462dba1f51dac35c
Yaroslav <example@gmail.com>
2020-06-13 06:31:15 +0300

    Установил prop-types и добавил валидацию пропсов для компонент

--- src/index.js
+++ src/index.js
@@ -8,11 +8,33 @@ const promoInfo = {
   year: 2014,
 };
 
+const MOVIE_TITLES = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`, `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`, `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`
+];
+
 ReactDOM.render(
     <App
       promoTitle={promoInfo.title}
       promoGenre={promoInfo.genre}
       promoYear={promoInfo.year}
+      movieTitles ={MOVIE_TITLES}
     />,
     document.querySelector(`#root`)
 );
