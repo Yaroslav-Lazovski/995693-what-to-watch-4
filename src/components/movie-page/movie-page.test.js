541274b996041f306fbefac910993b544a919cfd
Yaroslav <example@gmail.com>
2020-07-01 15:51:20 +0300

    Обновил тесты

--- src/components/movie-page/movie-page.test.js
+++ src/components/movie-page/movie-page.test.js
@@ -15,7 +15,27 @@ const movie = {
   description: `Movie description`,
   director: `Movie director`,
   starring: `Movie starring`,
+  runTime: `Movie runTime`
 };
+
+const reviews = [
+  {
+    id: 1,
+    author: `Kate Muir`,
+    date: `December 24, 2016`,
+    rating: 8.9,
+    text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years`,
+  },
+  {
+    id: 2,
+    author: `Bill Goodykoontz`,
+    date: `November 18, 2015`,
+    rating: 8.0,
+    text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+  },
+];
+
+const activeTab = `Overview`;
 
 
 describe(`MoviePage Snapshot`, () => {
@@ -32,7 +52,11 @@ describe(`MoviePage Snapshot`, () => {
         movieDescription={movie.description}
         movieDirector={movie.director}
         movieStarring={movie.starring}
+        movieRunTime={movie.runTime}
         movies={films}
+        reviews={reviews}
+        activeTab={activeTab}
+        renderTabs={() => {}}
         onTitleClick={() => {}}
         onPosterClick={() => {}}
       />)
