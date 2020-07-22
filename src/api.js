929f10a67e74c2b7ac4f84da059ab9659dcdab96
Yaroslav <example@gmail.com>
2020-07-22 14:07:03 +0300

    Создал api

---
+++ src/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+
+const URL = `https://4.react.pages.academy/wtw`;
+const TIMEOUT = 5000;
+const Error = {
+  UNAUTHORIZED: 401
+};
+
+export const createAPI = (onUnauthorized) => {
+  const api = axios.create({
+    baseURL: URL,
+    timeout: TIMEOUT,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => {
+    return response;
+  };
+
+  const onFail = (err) => {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      onUnauthorized();
+
+      throw err;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
